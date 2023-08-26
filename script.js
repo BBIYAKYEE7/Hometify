42ea3da674f5ab2e68d0d198929c6734c0a185ca
BBIYAKYEE7 <dadalgojr@gmail.com>
2023-08-26 23:12:49 +0900

    Final Code

---
+++ script.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal({ reset: true });
+
+			sr.reveal(".page1", {
+			origin: "top",
+			duration: 500,
+			distance: "200px",
+			});
+
+			sr.reveal(".logo-img", {
+			origin: "top",
+			duration: 500,
+			distance: "200px",
+			});
+
+			sr.reveal(".logo", {
+			origin: "right",
+			duration: 500,
+			distance: "200px",
+			});
+
+			sr.reveal(".logo-des", {
+			origin: "bottom",
+			duration: 500,
+			distance: "200px",
+			});
